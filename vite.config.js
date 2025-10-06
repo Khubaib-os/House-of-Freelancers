a5d5945dd202c6015d0888a457cd8f57706398a9
Haseeb Farooq <haseebfarooq35@gmail.com>
2025-10-06 19:00:48 +0500

    Update config for build

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,3 @@ export default defineConfig({
   plugins: [tailwindcss(),
     react()],
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//     react(),
-//   ],
-//   css: {
-//     // ✅ Disable LightningCSS to fix Vercel build error
-//     transformer: 'postcss',
-//     lightningcss: false,
-//   },
-// })
